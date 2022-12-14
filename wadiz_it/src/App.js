c7e20dd572f3779a069fc3bf271bc2a8a33f1957
owohaha123 <owohaha123@gmail.com>
2022-12-14 16:43:09 +0900

    Update App.js

--- wadiz_it/src/App.js
+++ wadiz_it/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Test from './components/Test';
+import Join from './components/Join';
+//import Home from './components/Home';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Join/> */}
+      
+      <Router>
+        {/* <Join/> */}
+        <Routes>
+          <Route path="/" element={<Join />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/join" element={<Join />} /> */}
+          <Route path="/Test" element={<Test />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
