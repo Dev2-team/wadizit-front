8c22e5d01ca76c37ce6f7981c72789fc14a3182f
owohaha123 <owohaha123@gmail.com>
2023-01-06 16:26:33 +0900

    logout confirm

--- wadiz_it/src/App.js
+++ wadiz_it/src/App.js
@@ -40,11 +40,8 @@ function App() {
     //세션에 저장된 로그인 아이디를 가져옴(로그인 상태 유지)
 
     const nickName = sessionStorage.getItem("nickName");
-
-    //console.log(mid);
     if (nickName !== null) {
       const newState = {
-
         logNick: nickName,
         flink: "/main",
       };
@@ -67,36 +64,39 @@ function App() {
 
   //로그아웃함수 (일반, 카카오)
   const onLogout = () => {
-    const CLIENT_ID = "3325b1fa29c94621b861b2793200c360";
-    const LOGOUT_REDIRECT_URI =  "http://localhost:3000";
-    const newState = {
-      logNick: "",
-      flink: "/login",
-    };
-    setLogState(newState);
+    if (window.confirm("로그아웃 하실?")) {
+      alert("로그아웃");
+      const CLIENT_ID = "3325b1fa29c94621b861b2793200c360";
+      const LOGOUT_REDIRECT_URI =  "http://localhost:3000";
+      const newState = {
+        logNick: "",
+        flink: "/login",
+      };
+      setLogState(newState);
     
-    //카카오계정과 함께 로그아웃하여 다시 로그인할 때 아이디 비밀번호 입력 필요
-    axios.get(`https://kauth.kakao.com/oauth/logout?client_id=${CLIENT_ID}&logout_redirect_uri=${LOGOUT_REDIRECT_URI}`).then((res)=>{
+      //카카오계정과 함께 로그아웃하여 다시 로그인할 때 아이디 비밀번호 입력 필요
+      axios.get(`https://kauth.kakao.com/oauth/logout?client_id=${CLIENT_ID}&logout_redirect_uri=${LOGOUT_REDIRECT_URI}`).then((res)=>{
       sessionStorage.removeItem("nickName");
       sessionStorage.removeItem("access_token");
-    })
-    // 토큰만 만료시켜 로그아웃하여 다시 로그인할 때 아이디 비밀번호 자동입력
-    // axios.post(`https://kapi.kakao.com/v1/user/logout`,null, {
-    //   headers : {
-    //     'Content-Type': 'application/x-www-form-urlencoded',
-    //     'Authorization' : `Bearer ${sessionStorage.getItem("access_token")}`
-    //   }
-    // }).then((res) => {
-    //   console.log(res);
-    // })
-    //로그아웃 시 로그인 상태 및 페이지번호 삭제
-
-    sessionStorage.removeItem("nickName");
-    sessionStorage.removeItem("memberNum");
-
-    // alert("로그아웃");
-    // sessionStorage.removeItem("pageNum");
-    nav("/"); //첫페이지로 돌아감.
+      })
+      // 토큰만 만료시켜 로그아웃하여 다시 로그인할 때 아이디 비밀번호 자동입력
+      // axios.post(`https://kapi.kakao.com/v1/user/logout`,null, {
+      //   headers : {
+      //     'Content-Type': 'application/x-www-form-urlencoded',
+      //     'Authorization' : `Bearer ${sessionStorage.getItem("access_token")}`
+      //   }
+      // }).then((res) => {
+      //   console.log(res);
+      // })
+      //로그아웃 시 로그인 상태 및 페이지번호 삭제
+      sessionStorage.removeItem("nickName");
+      sessionStorage.removeItem("memberNum");
+      // alert("로그아웃");
+      // sessionStorage.removeItem("pageNum");
+      nav("/"); //첫페이지로 돌아감.
+    } else {
+      alert("취소");
+    }
   };
 
   const setKakaoData = useCallback((data) => {
@@ -114,7 +114,6 @@ function App() {
 
   const onMypage = () => {
     const nickName = sessionStorage.getItem("nickName");
-
     const newState = {
       logNick: nickName,
       flink: "/myPage",
@@ -143,7 +142,6 @@ function App() {
         {/* <Route path="/adminPage1" element={<AdminPage1 />} />
         <Route path="/simpleSlider" element={<SimpleSlider />} />
         <Route path="/KakaoPayApprove" element={<KakaoPayApprove />} /> */}
-
 
         <Route path="/myPage" element={<MyPage />} />
         <Route path="/boardWrite" element={<BoardWrite/>}/>
