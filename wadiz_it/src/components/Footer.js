a36d2f704ee6c5a1d12f511e9ef3a4b6644d87ee
ICIA20220729T5AR <ICIA20220729T5AR@gmail.com>
2023-01-09 23:29:00 +0900

    Footer 추가

---
+++ wadiz_it/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Segment, Menu, Header } from "semantic-ui-react";
+
+const Footer = () => {
+  return (
+    <Segment style={{ marginTop: 10, height: 150 }} attached="bottom">
+      <Header>Footer</Header>
+    </Segment>
+  );
+};
+
+export default Footer;
