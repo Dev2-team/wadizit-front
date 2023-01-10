246c6a497c780c3bdab3afeb05ac1bff470ee919
ICIA20220729T5AR <ICIA20220729T5AR@gmail.com>
2023-01-10 16:47:06 +0900

    굿즈 리스트 컴포넌트 추가

---
+++ wadiz_it/src/components/GoodsList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Header } from "semantic-ui-react";
+import FundingList from "./FundingList";
+
+const GoodsList = () => {
+  return (
+    <Container textAlign="left">
+      <Header as="h3" dividing>
+        굿즈
+      </Header>
+    </Container>
+  );
+};
+
+export default GoodsList;
